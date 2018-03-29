504743732be6ee78efac29ab6da0913f2a29a6c6
Chen Weiguang <chen.weiguang@gmail.com>
2018-03-29 14:13:28 +0800

    Clean up the import run.sh

--- grafana/imports/run.sh
+++ grafana/imports/run.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env sh
 USER=admin
 PW=admin
-RETRY_COUNT=2
 
-HOSTNAME=grafana
-ELASTIC_HOSTNAME=elasticsearch
-PORT=3000
+RETRY_COUNT=2
+GRAFANA_URL=http://grafana:3000
+ELASTICSEARCH_URL=http://elasticsearch:9200
 
 is_valid_health_status() {
     status=`curl -s -u $USER:$PW -H "Content-Type: application/json" \
-        -X GET http://$HOSTNAME:$PORT/api/health \
+        -X GET $GRAFANA_URL/api/health \
         --retry $RETRY_COUNT | jq -r '.database'`
     echo "$status"
 }
@@ -23,24 +22,60 @@ done
 
 # for index logstash-*
 curl -s -i -u $USER:$PW -H "Content-Type: application/json" \
-    -X POST http://$HOSTNAME:$PORT/api/datasources \
-    -d '{"name":"elasticsearch","type":"elasticsearch","url":"http://'$ELASTIC_HOSTNAME':9200", "access":"proxy", "database":"logstash-*","jsonData":{"timeField":"datetime"}}' \
+    -X POST $GRAFANA_URL/api/datasources \
+    -d '
+    {
+        "name":"elasticsearch",
+        "type":"elasticsearch",
+        "url":"'$ELASTICSEARCH_URL'",
+        "access":"proxy",
+        "database":"logstash-*",
+        "jsonData":{"timeField":"datetime"}
+    }' \
     --retry $RETRY_COUNT > /dev/null
 
 # for index docker
 curl -s -i -u $USER:$PW -H "Content-Type: application/json" \
-    -X POST http://$HOSTNAME:$PORT/api/datasources \
-    -d '{"name":"elasticsearch-docker","type":"elasticsearch","url":"http://'$ELASTIC_HOSTNAME':9200", "access":"proxy", "database":"docker","jsonData":{"timeField":"@timestamp"}}' \
+    -X POST $GRAFANA_URL/api/datasources \
+    -d '
+    {
+        "name":"elasticsearch-docker",
+        "type":"elasticsearch",
+        "url":"'$ELASTICSEARCH_URL'",
+        "access":"proxy",
+        "database":"docker",
+        "jsonData":{"timeField":"@timestamp"}
+    }' \
     --retry $RETRY_COUNT > /dev/null
 
 # import storage monitoring dashboard
 curl -s -i -u $USER:$PW -H "Content-Type: application/json" \
-    -X POST http://$HOSTNAME:$PORT/api/dashboards/import \
-    -d '{"overwrite":true,"inputs":[{"name":"DS_ELASTICSEARCH","type":"datasource","pluginId":"elasticsearch","value":"elasticsearch"}],"dashboard":'"`cat /monitoring-dashboard.json | jq . -c`"'}' \
+    -X POST $GRAFANA_URL/api/dashboards/import \
+    -d '
+    {
+        "overwrite":true,
+        "inputs":[{
+            "name":"DS_ELASTICSEARCH",
+            "type":"datasource",
+            "pluginId":"elasticsearch",
+            "value":"elasticsearch"
+        }],
+        "dashboard":'"`cat /monitoring-dashboard.json | jq . -c`"'
+    }' \
     --retry $RETRY_COUNT > /dev/null
 
 # import docker logs dashboard
 curl -s -i -u $USER:$PW -H "Content-Type: application/json" \
-    -X POST http://$HOSTNAME:$PORT/api/dashboards/import \
-    -d '{"overwrite":true,"inputs":[{"name":"DS_ELASTICSEARCH-DOCKER","type":"datasource","pluginId":"elasticsearch","value":"elasticsearch-docker"}],"dashboard":'"`cat /docker-logs.json | jq . -c`"'}' \
+    -X POST $GRAFANA_URL/api/dashboards/import \
+    -d '
+    {
+        "overwrite":true,
+        "inputs":[{
+            "name":"DS_ELASTICSEARCH-DOCKER",
+            "type":"datasource",
+            "pluginId":"elasticsearch",
+            "value":"elasticsearch-docker"
+        }],
+        "dashboard":'"`cat /docker-logs.json | jq . -c`"'
+    }' \
     --retry $RETRY_COUNT > /dev/null
